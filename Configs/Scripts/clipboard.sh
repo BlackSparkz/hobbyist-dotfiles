28d6dc39ed1ee9c80ea897ac4cb014974f665dfb
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-05-19 17:15:16 +0530

    wezterm and bashrc added

--- Configs/Scripts/clipboard.sh
+++ Configs/Scripts/clipboard.sh
@@ -4,9 +4,7 @@ cliphist list | fzf \
   --prompt=" Search clipboard: " \
   --reverse \
   --border \
-  --preview 'echo {} | cliphist decode 2>/dev/null' \
-  --preview-window=right:40%:wrap \
-  --header ' Enter: copy | Ctrl+D: delete | Ctrl+A: clear all | Esc: quit' \
+  --header ' Enter: Copy | Ctrl+D: Delete | Ctrl+A: Clear all | Esc: Quit' \
   --bind 'ctrl-d:execute(echo {} | cliphist delete)+reload(cliphist list)' \
   --bind 'ctrl-a:execute(cliphist wipe)+reload(cliphist list)' \
   | cliphist decode | wl-copy
