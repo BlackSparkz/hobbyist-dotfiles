093a4345b87c951905c1f7152b83436fecc159e3
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-06-04 11:17:36 +0530

    Configs updated

--- Configs/hypr/hyprland_modules/Input.lua
+++ Configs/hypr/hyprland_modules/Input.lua
@@ -6,7 +6,7 @@ hl.config({
     input = {
         kb_layout  = "us",
         numlock_by_default = false,
-        repeat_rate = 60,
+        repeat_rate = 100,
         repeat_delay = 300,
         follow_mouse = 1,
         sensitivity = 1.0,
