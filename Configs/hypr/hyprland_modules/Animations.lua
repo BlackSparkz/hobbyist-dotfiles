4cbae8faec005ea77893648ee74f55789296955d
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-06-21 13:20:08 +0530

    End4 Anime added

--- Configs/hypr/hyprland_modules/Animations.lua
+++ Configs/hypr/hyprland_modules/Animations.lua
@@ -6,11 +6,9 @@ hl.config({
 
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
-hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
 hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
 hl.curve("quick",          { type = "bezier", points = { {0.1, 0},     {0.0, 1}     } })
 
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 hl.curve("hobbyist",       { type = "spring", mass = 1, stiffness = 40, dampening = 6 } )
 hl.curve("cat",            { type = "spring", mass = 1, stiffness = 30, dampening = 6 } )
 
@@ -19,13 +17,13 @@ hl.animation({ leaf = "border",        enabled = true,  speed = 1,    bezier = "
 hl.animation({ leaf = "windows",       enabled = true,  speed = 10,   spring = "cat",        style = "slide" })
 hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 10,   spring = "cat",        style = "slide" })
 hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 10,   spring = "cat",        style = "slide bottom" })
-hl.animation({ leaf = "windowsMove",   enabled = true,  speed = 10,   spring = "hobbyist" })
+hl.animation({ leaf = "windowsMove",   enabled = true,  speed = 10,   spring = "cat" })
 hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
 hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
 hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
 hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "slide bottom" })
-hl.animation({ leaf = "layersOut",     enabled = true,  speed = 3,    bezier = "linear",       style = "slide bottom" })
+hl.animation({ leaf = "layersOut",     enabled = true,  speed = 4,    bezier = "almostLinear", style = "slide bottom" })
 hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
 hl.animation({ leaf = "workspaces",    enabled = true,  speed = 12,   spring = "hobbyist",     style = "slidevert" })
