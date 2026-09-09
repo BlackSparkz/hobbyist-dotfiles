2493e8507627d281f7eb80e916c18e61e3979768
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-09-09 09:37:35 +0530

    configs updated

--- Configs/fish/config.fish
+++ Configs/fish/config.fish
@@ -6,6 +6,7 @@ set -g fish_greeting ""
 alias c='clear'
 alias cat='bat'
 alias e='exit'
+alias q='exit'
 alias reload='source ~/.config/fish/config.fish ; kitty @ load-config'
 alias ls="eza -1h -s modified -r --icons=always --group-directories-first"
 alias bip="pacman -Qqe > ~/hobbyist-dotfiles/Configs/installed-pkg/pkglist.txt && echo 'Package names backed up'"
