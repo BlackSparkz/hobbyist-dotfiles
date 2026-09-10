2a183ac2526f8f206970468528e1c993a8d55194
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-09-10 10:20:30 +0530

    configs updated

--- Configs/Scripts/clipboard.sh
+++ Configs/Scripts/clipboard.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cliphist list | fzf \
-  --prompt=" Search clipboard: " \
+  --prompt="Search clipboard: " \
   --reverse \
   --border \
-  --header ' Enter: Copy | Ctrl+D: Delete | Ctrl+A: Clear all | Esc: Quit' \
+  --header 'Enter: Copy | Ctrl+D: Delete | Ctrl+A: Clear all | Ctrl+Q: QR | Esc: Quit' \
   --bind 'ctrl-d:execute(echo {} | cliphist delete)+reload(cliphist list)' \
   --bind 'ctrl-a:execute(cliphist wipe)+reload(cliphist list)' \
+  --bind 'ctrl-q:execute-silent(cliphist decode {} | qrencode -o /tmp/clip-qr.png -s 10 -m 2 -t PNG && timeout 10 swayimg /tmp/clip-qr.png)' \
   | cliphist decode | wl-copy
