baab41ef957374c567c1fbf03d6dad3336df196c
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-09-10 11:08:13 +0530

    configs updated

--- Configs/Scripts/rofi_clipboard.sh
+++ Configs/Scripts/rofi_clipboard.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 pkill rofi || \
-cliphist list | \
+selected=$(cliphist list | \
 rofi -dmenu \
-     -display-columns 2 \
-     -theme \~/.config/rofi/clipboard.rasi | \
-cliphist decode | \
-wl-copy
+    -display-columns 2 \
+    -theme ~/.config/rofi/clipboard.rasi \
+    -kb-custom-1 "ctrl+q")
+
+exit_code=$?
+
+if [ "$exit_code" -eq 10 ]; then
+    # ctrl+q pressed
+    cliphist decode <<< "$selected" | qrencode -o /tmp/clip-qr.png -s 6 -m 2 -t PNG
+    timeout 8 swayimg /tmp/clip-qr.png
+else
+    cliphist decode <<< "$selected" | wl-copy
+fi
