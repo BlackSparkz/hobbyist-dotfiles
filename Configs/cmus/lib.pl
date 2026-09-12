1561e68d39e63736ba16c85d204b8e8b56ccb87e
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-09-12 19:20:09 +0530

    QR added

---
+++ Configs/cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/blackspark/Music/2_5190768367228725358.mp3
+/home/blackspark/Music/01 - Saiyaara.m4a
+/home/blackspark/Music/02 - Pal Pal Dil Ke Paas - Title Track.m4a
+/home/blackspark/Music/02 - Banjaara.m4a
+/home/blackspark/Music/01 - Galliyan.m4a
