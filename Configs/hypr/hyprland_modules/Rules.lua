17416313d59e76ea36f73ad86f4693afbb8b888e
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-09-14 21:51:42 +0530

    uninstall script updated

--- Configs/hypr/hyprland_modules/Rules.lua
+++ Configs/hypr/hyprland_modules/Rules.lua
@@ -67,7 +67,7 @@ hl.window_rule({
   match = { class = "dashboard" },
   float = true,
   no_initial_focus = false,
-  move = {1260,750},
+  move = {1260,730},
   opacity = "1.0",
   pin = true,
   size = {"(monitor_w*0.33)","(monitor_h*0.30)"},
